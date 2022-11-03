c18d56ac2b67f4144dce286a8c099bfacbe88547
Sahil <kardilesahil@gmail.com>
2022-11-03 08:39:41 +0530

    [Sahil] problems solved

---
+++ day12shellscript.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -x
+
+read -p "Enter firstNum: " firstNum
+read -p "Enter secondNum: " secondNum
+
+echo "sum of two nunmbers: " $(($firstNum+$secondNum))
+
+echo "substraction of two numbers: " $(($firstNum+$secondNum))
+
+echo "product of two numbers: " $(($firstNum*$secondNum))
+
+echo "division of two numbers: " $(($firstNum/$secondNum))
